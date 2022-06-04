41234a80520aa16e7e4b7bcbb0ad306f295fc097
Utsav Shrestha <utsavstha@outlook.com>
2022-06-04 17:16:19 +0545

    new additions

--- lib/utils/api_response.dart
+++ lib/utils/api_response.dart
@@ -1,5 +1,5 @@
 /// A template class being used to listen for and hold api responses, states of this
-/// class is mainly responsible for showing loading dialog and finally response
+/// class is mainly responsible for showingloading dialog and finally response
 /// during and after api calls
 class ApiResponse {
   dynamic model;
