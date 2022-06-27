37db1dc90a2dcebf12dc7ed1cbed779ce12d8a57
Utsav Shrestha <utsavstha@outlook.com>
2022-06-27 22:30:30 +0545

    new features

--- lib/ui/dashboard/dashboard.dart
+++ lib/ui/dashboard/dashboard.dart
@@ -31,7 +31,7 @@ class _DashboardState extends ConsumerState<Dashboard> {
   }
 
   _fetchData() {
-    // WidgetsBinding.nstance!.addPostFrameCallback((timeStamp) {
+    // WidgetsBinding.nstce!.addPostFrameCallback((timeStamp) {
     ref.read(dashboardNotifierProvider).pollTables();
     // });
   }
